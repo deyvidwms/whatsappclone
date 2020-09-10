edb560a7a6c3b7ec61a9ed5b41a35e6f0edc2511
Deyvid William <deyvidwms@gmail.com>
2020-09-10 11:18:51 -0300

    Exibindo informações da sidebar de forma dinâmica

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './index.css'
 
-export default ({onClick, active}) => {
+export default ({onClick, active, data}) => {
   return (
     <div 
       className={`chatListItem ${active? 'active': ''}`}
       onClick={onClick}
     > 
     
-      <img className="chatListItem--avatar" src="https://www.w3schools.com/howto/img_avatar.png" alt=""/>
+      <img className="chatListItem--avatar" src={data.image} alt=""/>
 
       <div className="chatListItem--lines">
 
         <div className="chatListItem--line">
 
-          <div className="chatListItem--name">Deyvid William</div>
+          <div className="chatListItem--name">{data.title}</div>
           <div className="chatListItem--date">19:00</div>
 
         </div>
